bbc26c50386fd9da6ba4a2abc4bc7736f87b0cae
Mikhail Sergeev <sergeevmikhail.dev@gmail.com>
2025-05-23 15:29:21 +0400

    final working repo

--- src/setup/pipes.setup.ts
+++ src/setup/pipes.setup.ts
@@ -1,14 +1,28 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { BadRequestDomainException } from '../core/exceptions/domain-exceptions';
 
 export function pipesSetup(app: INestApplication) {
   //Глобальный пайп для валидации и трансформации входящих данных.
   //На следующем занятии рассмотрим подробнее
   app.useGlobalPipes(
     new ValidationPipe({
-      //class-transformer создает экземпляр dto
-      //соответственно применятся значения по-умолчанию
-      //и методы классов dto
+      stopAtFirstError: true,
       transform: true,
+      exceptionFactory: (errors) => {
+        const errorsForResponse = [];
+        errors.forEach((e) => {
+          // @ts-ignore
+          const constraintsKeys = Object.keys(e.constraints);
+          constraintsKeys.forEach((constraintsKey) => {
+            // @ts-ignore
+            errorsForResponse.push({
+              message: e.constraints![constraintsKey],
+              key: e.property,
+            });
+          });
+        });
+        throw new BadRequestDomainException(errorsForResponse);
+      },
     }),
   );
 }
